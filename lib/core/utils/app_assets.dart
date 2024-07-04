c7725232db69a9692c77d3a8a51bc1e9f12b31d6
haitham <saadh7144@gmail.com>
2024-07-04 22:21:11 +0300

    appcolors and images

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static String basePath = 'assets/images/';
+  static String lock = '${basePath}lock.png';
+  static String background = '${basePath}background.png';
+  static String backgroundtwo = '${basePath}backgroundtwo.png';
+  static String chef = '${basePath}chef.png';
+  static String profile = '${basePath}profile.png';
+}
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color(0xffFA9A0C);
+  static Color black = const Color(0xff1E1E1E);
+  static Color white = const Color(0xffFFFFFF);
+  static Color gray = const Color(0xff9D9FA0);
+}
