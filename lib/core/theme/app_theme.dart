e22b9d207badd567f6adf962365ea1d69649913e
haitham <saadh7144@gmail.com>
2024-07-06 10:35:58 +0300

    app theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:chef_app/core/utils/app_text_style.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData getAppTheme() {
+  return ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primary,
+    ),
+    textTheme:
+        TextTheme(displayLarge: boldStyle(), displayMedium: regularStyle()),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      hintStyle: boldStyle(color: AppColors.gray, fontSize: 16),
+    ),
+  );
+}
--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primary = const Color(0xffFA9A0C);
-  static Color black = const Color(0xff1E1E1E);
-  static Color white = const Color(0xffFFFFFF);
-  static Color gray = const Color(0xff9D9FA0);
+  static const Color primary = Color(0xffFA9A0C);
+  static const Color black = Color(0xff1E1E1E);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color gray = Color(0xff9D9FA0);
 }
